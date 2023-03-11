df9867dac237c8683af90f6c8d7f46d56678341c
Bec <dilella.r@gmail.com>
2023-03-12 09:42:01 +1100

    temp hide discord link

--- src/Logo.svelte
+++ src/Logo.svelte
@@ -4,33 +4,33 @@
       <img src = "Full_Wordmark_White.svg" alt="Elegant Skeleton" width="160.25" height="170"/>
     </a>
   </h1>
-  <h2>Public Aptitude Challenge<br>
-    <span><a href="https://discord.gg/g5Yhybq6G9">join the discord</a></span></h2>
+  <h2>Public Aptitude Challenge</h2>
+  <!-- <h4><a href="https://discord.gg/g5Yhybq6G9">join the discord</a></h4> -->
 </div>
 
 <style lang="scss">
   h1,
   h2 {
     text-align: left;
+    
   }
 
   h1 {
     font-size: 28px;
     float: left;
-    padding-left: 10%;
+    padding-left: 8%;
   }
 
   h2 {
     font-size: 20px;
-    padding-top: 15%;
-    padding-bottom: 8%;
+    padding-top: 22%;
+    margin-bottom: 15%;
     
   }
 
-   span {
+/*   h4 {
     font-size: 15px;
     text-align: center;
-    display: inline-block;
-    padding-top: 0.8em;
-  } 
+    margin: 2;
+  } */
 </style>
