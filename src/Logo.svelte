e6ec786bf4960533f85ed6a206debad5a83942dc
Fluto <mesut.l@hotmail.com>
2023-03-12 12:12:46 +1100

    added discord logo, adjusted styling

--- src/Logo.svelte
+++ src/Logo.svelte
@@ -1,36 +1,52 @@
 <div class="logo-container">
   <h1>
-    <a href = "#top" >
-      <img src = "Full_Wordmark_White.svg" alt="Elegant Skeleton" width="160.25" height="170"/>
+    <a href="#top">
+      <img
+        src="Full_Wordmark_White.svg"
+        alt="Elegant Skeleton"
+        width="160.25"
+        height="170"
+      />
     </a>
   </h1>
-  <h2>Public Aptitude Challenge</h2>
-  <!-- <h4><a href="https://discord.gg/g5Yhybq6G9">join the discord</a></h4> -->
+  <div class="title-group">
+    <h2>Public Aptitude Challenge</h2>
+    <h4><a href="https://discord.gg/g5Yhybq6G9">join the discord</a></h4>
+  </div>
 </div>
 
 <style lang="scss">
+  @import './styles/constants';
+  .logo-container {
+    display: grid;
+    grid-template-columns: 200px 1fr;
+    align-items: center;
+  }
   h1,
   h2 {
     text-align: left;
-    
   }
 
   h1 {
     font-size: 28px;
     float: left;
-    padding-left: 8%;
   }
 
   h2 {
     font-size: 20px;
-    padding-top: 22%;
-    margin-bottom: 15%;
-    
+    margin: 0;
   }
 
-/*   h4 {
+  h4 {
     font-size: 15px;
-    text-align: center;
-    margin: 2;
-  } */
+    margin: 0;
+    color:$dark-accent-color;
+  }
+  .title-group {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    gap: 10px;
+  }
 </style>
